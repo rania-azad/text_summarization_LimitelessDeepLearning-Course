a54e428b6509b4d04a915d31dd0b55a4bb806973
Hadjer-Benmeziane <hadjer.benmeziane@ibm.com>
2024-02-15 21:25:39 +0100

    Revert "add project files"
    This reverts commit 68fb6ec7c35ac187f39aea83a72655a5eb20d1d5.

--- src/utils_data.py
+++ src/utils_data.py
@@ -15,16 +15,14 @@ from torchvision import transforms, utils
 class GPT2Dataset(Dataset):
   def __init__(self, df):
     self.encodings = df['encodings'].to_list()
-    print(self.encodings)
     self.sum_idx = df['text_len'].to_list()
   
   def __len__(self):
     return len(self.sum_idx)
 
   def __getitem__(self, idx):
-    input_ids = self.encodings[idx]["input_ids"]
-    text = torch.tensor(input_ids)# exract input_ids from encodings
-    attn_mask = torch.tensor(self.encodings.iloc[idx]['attention_mask']) # exract mask from encodings
+    text = torch.tensor() # exract input_ids from encodings
+    attn_mask = torch.tensor() # exract mask from encodings
     s_idx = self.sum_idx[idx] + 2 # add bos and cls
     out = {'text': text, 'mask': attn_mask, 's_idx': s_idx}
     return out 
--- src/utils_tokenizer.py
+++ src/utils_tokenizer.py
@@ -1,14 +1,15 @@
 # Utility functions for tokenization 
-def apply_tokenizer(tokenizer, text, summary, max_len):
+def apply_tokenizer(tokenizer, text, sum, max_len):
 	# TODO: Complete this function 
 	# Concatenate text and summary and in-between add a special token 
 	# <BOS> is beginning of sentence 
 	# <EOS> is end of sentence
-	return tokenizer('<BOS>' + text +  '<SUMMARIZE>' + summary + '<EOS>', truncation=True, max_length=max_len, padding="max_length")
+	return tokenizer('<BOS>'  '<EOS>', truncation=True, max_length=max_len, padding="max_length")
 
 def tokenize_text(tokenizer, df, max_len):
     df['encodings'] = df.apply(lambda x : apply_tokenizer(tokenizer, x['text'], x['summary'], max_len), axis=1)
     #encodings is dict type: contains input_ids and attention_mas
+    
     del df['text']
     del df['summary']
 
