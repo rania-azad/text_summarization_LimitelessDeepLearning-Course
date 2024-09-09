124d6e473e953e5f6ae07adebebc5113aca98f7f
Rania Azad <75755469+rania-azad@users.noreply.github.com>
2024-09-10 00:03:40 +0300

    Update utils_tokenizer.py

--- src/utils_tokenizer.py
+++ src/utils_tokenizer.py
@@ -1,12 +1,22 @@
 # Utility functions for tokenization 
-def apply_tokenizer(tokenizer, text, sum, max_len):
+#def apply_tokenizer(tokenizer, text, sum, max_len):
 	# TODO: Complete this function 
 	# Concatenate text and summary and in-between add a special token 
 	# <BOS> is beginning of sentence 
 	# <EOS> is end of sentence
-	  ts =  text + ' <SUMMARY> ' + sum
+	# ts =  text + ' <SUMMARY> ' + sum
     #encodings = tokenizer('<BOS> ' + ts + ' <EOS>', truncation=True, max_length=max_len, padding="max_length")
-	return tokenizer('<BOS>'  '<EOS>', truncation=True, max_length=max_len, padding="max_length")
+	#return tokenizer('<BOS>'  '<EOS>', truncation=True, max_length=max_len, padding="max_length")
+
+def apply_tokenizer(tokenizer, text, summary, max_len):
+    # Concatenate text and summary, with a special token in between
+    ts = text + ' <SUMMARY> ' + summary
+    
+    # Tokenize the concatenated string with special BOS and EOS tokens
+    encodings = tokenizer('<BOS> ' + ts + ' <EOS>', truncation=True, max_length=max_len, padding="max_length")
+    
+    return encodings
+
 
 def tokenize_text(tokenizer, df, max_len):
     df['encodings'] = df.apply(lambda x : apply_tokenizer(tokenizer, x['text'], x['summary'], max_len), axis=1)
